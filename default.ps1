45fe2c1dfaf24bb83e2bafb26caf99075d44adba
Damian Hickey <dhickey@gmail.com>
2014-08-26 08:45:31 +0200

    Fix ILMerge build task. This is what happens when I commit a change to the build script without testing it first.

--- default.ps1
+++ default.ps1
@@ -41,15 +41,16 @@ task RunTests -depends Compile {
 
 task ILMerge -depends Compile {
 	New-Item $buildOutputDir -Type Directory -ErrorAction SilentlyContinue
-	
-	$input_dlls = "$dllDir\Cedar.dll "
+	$dllDir = "$srcDir\Cedar\bin\Release"
+	$inputDlls = "$dllDir\Cedar.dll"
 	@("Microsoft.Owin", "NewtonSoft.Json", "Owin", "System.Reactive.Core", "System.Reactive.Interfaces", "System.Reactive.Linq",`
-		"System.Reactive.PlatformServices") |% { $input_dlls = "$input_dlls $dllDir\$_.dll" }
-	Invoke-Expression "$ilmerge_path /targetplatform:v4 /internalize /allowDup /target:library /out:$buildOutputDir\Cedar.dll $input_dlls"
+		"System.Reactive.PlatformServices") |% { $inputDlls = "$inputDlls $dllDir\$_.dll" }
+	Invoke-Expression "$ilmerge_path /targetplatform:v4 /internalize /allowDup /target:library /out:$buildOutputDir\Cedar.dll $inputDlls"
 	
-	$input_dlls = "$dllDir\Cedar.Client.dll "
-	@("NewtonSoft.Json") |% { $input_dlls = "$input_dlls $dllDir\$_.dll" }
-	Invoke-Expression "$ilmerge_path /targetplatform:v4 /internalize /allowDup /target:library /out:$buildOutputDir\Cedar.Client.dll $input_dlls"
+	$dllDir = "$srcDir\Cedar.Client\bin\Release"
+	$inputDlls = "$dllDir\Cedar.Client.dll "
+	@("NewtonSoft.Json") |% { $inputDlls = "$input_dlls $dllDir\$_.dll" }
+	Invoke-Expression "$ilmerge_path /targetplatform:v4 /internalize /allowDup /target:library /out:$buildOutputDir\Cedar.Client.dll $inputDlls"
 }
 
 task CreateNuGetPackages -depends ILMerge {
