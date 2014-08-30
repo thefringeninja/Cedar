2084e30d5992310b087fb1bc69e14c1ea224dd9c
Damian Hickey <dhickey@gmail.com>
2014-08-30 13:10:05 +0200

    Tweek DefaultAggregateFactory to construct Aggregates with a public ctor that takes the stream / AR Id.

--- src/Cedar/Domain/DefaultAggregateFactory.cs
+++ src/Cedar/Domain/DefaultAggregateFactory.cs
@@ -4,12 +4,15 @@ namespace Cedar.Domain
     using System.Reflection;
     using Cedar.Domain.Persistence;
 
+    /// <summary>
+    /// Can construct aggregates that have a public constructor that take the aggregate Id as a string.
+    /// </summary>
     public class DefaultAggregateFactory : IAggregateFactory
     {
         public IAggregate Build(Type type, string id)
         {
             ConstructorInfo constructor = type.GetConstructor(
-                BindingFlags.NonPublic | BindingFlags.Instance, null, new[] {typeof (string) }, null);
+                BindingFlags.Public | BindingFlags.Instance, null, new[] {typeof (string) }, null);
 
             return constructor.Invoke(new object[] {id}) as IAggregate;
         }
