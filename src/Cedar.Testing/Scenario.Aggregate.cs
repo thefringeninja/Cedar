d51ff1a4aa2c6b84f6a804bac276057582473fa8
thefringeninja <joao.paul@braganca.name>
2014-09-02 12:16:48 +0200

    scenarios for aggregates and value objects

---
+++ src/Cedar.Testing/Scenario.Aggregate.cs
@@ -0,0 +1,105 @@
+﻿namespace Cedar.Testing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Cedar.Domain;
+    using FluentAssertions;
+
+    public static partial class Scenario
+    {
+        public static IAggregateGiven<T> ForAggregate<T>() where T : IAggregate
+        {
+            return ForAggregate<T>("testid");
+        }
+
+        public static IAggregateGiven<T> ForAggregate<T>(string aggregateId) where T : IAggregate
+        {
+            var factory = new DefaultAggregateFactory();
+            var aggregate = (T) factory.Build(typeof (T), aggregateId);
+            return new AggregateScenarioBuilder<T>(aggregate);
+        }
+
+        public static IAggregateGiven<T> ForAggregate<T>(T aggregate) where T : IAggregate
+        {
+            return new AggregateScenarioBuilder<T>(aggregate);
+        }
+
+        private class AggregateScenarioBuilder<T> : IAggregateGiven<T> where T : IAggregate
+        {
+            private readonly T _aggregate;
+            private Action _given = () => { };
+            private Action _when = () => { };
+
+            public AggregateScenarioBuilder(T aggregate)
+            {
+                _aggregate = aggregate;
+            }
+
+            public IAggregateWhen<T> Given(params object[] events)
+            {
+                _given = () =>
+                {
+                    foreach (object @event in events)
+                    {
+                        _aggregate.ApplyEvent(@event);
+                    }
+                };
+                return this;
+            }
+
+            public IAggregateThen When(Action<T> when)
+            {
+                _when = () => when(_aggregate);
+                return this;
+            }
+
+            public void Then(params object[] expectedEvents)
+            {
+                _given();
+                _when();
+
+                var uncommittedEvents =
+                    new List<object>(_aggregate.GetUncommittedEvents().Cast<object>());
+                uncommittedEvents.ShouldBeEquivalentTo(expectedEvents);
+            }
+
+            public void ThenNothingHappened()
+            {
+                _given();
+                _when();
+
+                var uncommittedEvents =
+                    new List<object>(_aggregate.GetUncommittedEvents().Cast<object>());
+                uncommittedEvents.Should().BeEmpty();
+            }
+
+            public void ThenShouldThrow<TException>() where TException : Exception
+            {
+                Action then = () => _when();
+
+                _given();
+                then.ShouldThrow<TException>();
+            }
+        }
+
+        public interface IAggregateGiven<out T> : IAggregateWhen<T> where T : IAggregate
+        {
+            IAggregateWhen<T> Given(params object[] events);
+        }
+
+        public interface IAggregateThen
+        {
+            void Then(params object[] expectedEvents);
+
+            void ThenNothingHappened();
+
+            void ThenShouldThrow<TException>() where TException : Exception;
+        }
+
+        public interface IAggregateWhen<out T> : IAggregateThen where T : IAggregate
+        {
+            IAggregateThen When(Action<T> when);
+        }
+    }
+}
---
+++ src/Cedar.Testing/Scenario.AnyObject.cs
@@ -0,0 +1,69 @@
+﻿namespace Cedar.Testing
+{
+    using System;
+    using FluentAssertions;
+
+    public static partial class Scenario
+    {
+        public static IGiven<T> For<T>()
+        {
+            return new AnyObjectScenarioBuilder<T>();
+        }
+
+        private class AnyObjectScenarioBuilder<T> : IGiven<T>
+        {
+            private Action _given = () => { };
+            private T _instance;
+            private T _result;
+
+            private Action _when;
+
+            public void ThenShouldEqual(T other)
+            {
+                _given();
+                _when();
+                _result.Should().Be(other);
+            }
+
+            public void ThenShouldThrow<TException>(Func<TException, bool> equals = null) where TException : Exception
+            {
+                equals = equals ?? (_ => true);
+                Action then = () => _when();
+
+                _given();
+                then.ShouldThrow<TException>()
+                    .Which.Should().Match<TException>(ex => equals(ex));
+            }
+
+            public IThen<T> When(Func<T, T> when)
+            {
+                _when = () => _result = when(_instance);
+
+                return this;
+            }
+
+            public IWhen<T> Given(T instance)
+            {
+                _given = () => _instance = instance;
+
+                return this;
+            }
+        }
+
+        public interface IGiven<T> : IWhen<T>
+        {
+            IWhen<T> Given(T instance);
+        }
+
+        public interface IThen<T>
+        {
+            void ThenShouldEqual(T other);
+            void ThenShouldThrow<TException>(Func<TException, bool> equals = null) where TException : Exception;
+        }
+
+        public interface IWhen<T> : IThen<T>
+        {
+            IThen<T> When(Func<T, T> when);
+        }
+    }
+}
