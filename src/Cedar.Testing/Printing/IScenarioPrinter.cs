09bef7645675f23ea47d8ef51b64500e93cf566b
Damian Hickey <dhickey@gmail.com>
2014-09-05 15:23:33 +0200

    Using Inflector nuget package.

--- src/Cedar.Testing/Printing/IScenarioPrinter.cs
+++ src/Cedar.Testing/Printing/IScenarioPrinter.cs
@@ -8,11 +8,15 @@
         Task<IDisposable> WriteHeader(string scenarioName, TimeSpan? duration, bool passed);
         
         Task WriteGiven(object given);
+
         Task WriteWhen(object when);
+
         Task WriteExpect(object expect);
+
         Task WriteOcurredException(Exception occurredException);
         
         Task WriteStartCategory(string category);
+
         Task WriteEndCategory(string category);
         
         Task Flush();
