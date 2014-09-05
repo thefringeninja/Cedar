d844706773d24289024914efccab012025f98c85
thefringeninja <joao.paul@braganca.name>
2014-09-05 16:42:17 +0200

    observable process manager spike

---
+++ src/Cedar/ProcessManager/IProcessManager.cs
@@ -0,0 +1,10 @@
+﻿namespace Cedar.ProcessManager
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface IProcessManager : IDisposable
+    {
+        Task RunProcess();
+    }
+}
