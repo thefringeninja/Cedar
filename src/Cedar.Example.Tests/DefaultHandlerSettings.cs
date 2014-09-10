31a7d92836d52abd2d83e0c3a23dd888757eaeb9
thefringeninja <joao.paul@braganca.name>
2014-09-10 16:30:31 +0200

    leveraging the query middleware in the example tests

--- src/Cedar.Example.Tests/DefaultHandlerSettings.cs
+++ src/Cedar.Example.Tests/DefaultHandlerSettings.cs
@@ -1,29 +1,22 @@
 namespace Cedar.Example.Tests
 {
-    using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Linq;
     using Cedar.Annotations;
-    using Cedar.Commands;
     using Cedar.ContentNegotiation;
     using Cedar.Handlers;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Serialization;
 
     internal class DefaultHandlerSettings : HandlerSettings
     {
-        private readonly JsonSerializer _jsonSerializer;
-
         internal DefaultHandlerSettings(
-            [NotNull] HandlerModule handlerModule,
+            [NotNull] IHandlerResolver handlerModule,
             [NotNull] IContentTypeMapper contentTypeMapper,
             IExceptionToModelConverter exceptionToModelConverter = null)
             : this(Enumerable.Repeat(handlerModule, 1), contentTypeMapper, exceptionToModelConverter)
         {}
 
         internal DefaultHandlerSettings(
-            [NotNull] IEnumerable<HandlerModule> handlerModules,
+            [NotNull] IEnumerable<IHandlerResolver> handlerModules,
             [NotNull] IContentTypeMapper contentTypeMapper,
             IExceptionToModelConverter exceptionToModelConverter = null)
             : base(handlerModules, contentTypeMapper, exceptionToModelConverter)
