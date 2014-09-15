127d87f708067c801cf812b23e1eb557c03b4462
thefringeninja <joao.paul@braganca.name>
2014-09-15 16:17:44 +0200

    having to remember what types to register is a pain
    so instead the specialized modules will remember what was registered.

--- src/Cedar/Queries/QueryHandlerModule.cs
+++ src/Cedar/Queries/QueryHandlerModule.cs
@@ -1,24 +1,30 @@
 ﻿namespace Cedar.Queries
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Cedar.Handlers;
 
-    public class QueryHandlerModule : IHandlerResolver
+    public class QueryHandlerModule : IHandlerResolver, IEnumerable<Type>
     {
         private readonly HandlerModule _inner;
-
+        private readonly ICollection<Type> _registeredTypes;
         public QueryHandlerModule()
         {
             _inner = new HandlerModule();
+            _registeredTypes = new Collection<Type>();
         }
         public void For<TInput, TOutput>(IQueryHandler<TInput, TOutput> handler, params Pipe<QueryMessage<TInput, TOutput>>[] pipeline)
         {
             pipeline.Aggregate(_inner.For<QueryMessage<TInput, TOutput>>(), (builder, pipe) => builder.Pipe(pipe),
                 BuildHandler(handler));
+
+            _registeredTypes.Add(typeof(TInput));
+            _registeredTypes.Add(typeof(TOutput));
         }
 
         private static Func<IHandlerBuilder<QueryMessage<TInput, TOutput>>, ICreateHandlerBuilder> BuildHandler<TInput, TOutput>(IQueryHandler<TInput, TOutput> handler)
@@ -36,6 +42,16 @@
         public IEnumerable<Handler<TMessage>> GetHandlersFor<TMessage>()
         {
             return _inner.GetHandlersFor<TMessage>();
+        }
+
+        public IEnumerator<Type> GetEnumerator()
+        {
+            return _registeredTypes.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
         }
     }
 }
