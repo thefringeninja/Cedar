65f1b010c58ca52157644edb8f7f1e4f4c8f17be
Damian Hickey <dhickey@gmail.com>
2014-09-24 15:11:48 +0200

    Simplify IRepository - move overloads to extension methods.

--- src/Cedar/Domain/Persistence/IRepository.cs
+++ src/Cedar/Domain/Persistence/IRepository.cs
@@ -6,17 +6,8 @@ namespace Cedar.Domain.Persistence
 
     public interface IRepository
     {
-        TAggregate GetById<TAggregate>(Guid id) where TAggregate : class, IAggregate;
-
-        TAggregate GetById<TAggregate>(Guid id, int version) where TAggregate : class, IAggregate;
-
-        TAggregate GetById<TAggregate>(string bucketId, Guid id) where TAggregate : class, IAggregate;
-
         TAggregate GetById<TAggregate>(string bucketId, Guid id, int version) where TAggregate : class, IAggregate;
 
-        Task Save(IAggregate aggregate, Guid commitId, Action<IDictionary<string, object>> updateHeaders);
-
-        Task Save(string bucketId, IAggregate aggregate, Guid commitId,
-            Action<IDictionary<string, object>> updateHeaders);
+        Task Save(string bucketId, IAggregate aggregate, Guid commitId, Action<IDictionary<string, object>> updateHeaders);
     }
 }
