1483941afec259004f0983464e2186f21f34c2b2
thefringeninja <joao.paul@braganca.name>
2014-09-26 11:14:51 +0200

    bringing dependencies forward

--- src/Cedar/DefaultHandlerSettings.cs
+++ src/Cedar/DefaultHandlerSettings.cs
@@ -4,6 +4,7 @@
     using Cedar.Annotations;
     using Cedar.ExceptionModels;
     using Cedar.Handlers;
+    using Cedar.Serialization.Client;
     using Cedar.TypeResolution;
 
     public class DefaultHandlerSettings : HandlerSettings
@@ -11,15 +12,17 @@
         public DefaultHandlerSettings(
            [NotNull] IHandlerResolver handlerModule,
            [NotNull] IRequestTypeResolver requestTypeResolver,
-           IExceptionToModelConverter exceptionToModelConverter = null)
-            : this(new[] { handlerModule }, requestTypeResolver, exceptionToModelConverter)
+           IExceptionToModelConverter exceptionToModelConverter = null,
+           ISerializer serializer = null)
+            : this(new[] { handlerModule }, requestTypeResolver, exceptionToModelConverter, serializer)
         {}
 
         public DefaultHandlerSettings(
             [NotNull] IEnumerable<IHandlerResolver> handlerModules,
             [NotNull] IRequestTypeResolver requestTypeResolver,
-            IExceptionToModelConverter exceptionToModelConverter = null)
-            : base(handlerModules, requestTypeResolver, exceptionToModelConverter)
+            IExceptionToModelConverter exceptionToModelConverter = null,
+           ISerializer serializer = null)
+            : base(handlerModules, requestTypeResolver, exceptionToModelConverter, serializer)
         {}
 
     }
