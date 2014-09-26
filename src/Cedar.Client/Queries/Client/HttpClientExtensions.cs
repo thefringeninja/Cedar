6d04a95211ea5f3fff5a09cbd8e1752f148c8aa5
thefringeninja <joao.paul@braganca.name>
2014-09-26 14:13:16 +0200

    allow override of default behavior

--- src/Cedar.Client/Queries/Client/HttpClientExtensions.cs
+++ src/Cedar.Client/Queries/Client/HttpClientExtensions.cs
@@ -10,26 +10,40 @@ namespace Cedar.Queries.Client
 
     public static class HttpClientExtensions
     {
-        public static async Task<TOutput> ExecuteQuery<TInput, TOutput>(this HttpClient client, TInput input, Guid queryId, IMessageExecutionSettings settings)
+        public static async Task<TOutput> ExecuteQuery<TInput, TOutput>(this HttpClient client, TInput input, Guid queryId, IMessageExecutionSettings settings, Func<object, Type, Type, IMessageExecutionSettings, HttpRequestMessage> getRequest = null)
         {
-            string queryJson = settings.Serializer.Serialize(input);
-            var httpContent = new StringContent(queryJson);
-            httpContent.Headers.ContentType =
-                MediaTypeHeaderValue.Parse("application/vnd.{0}.{1}+json".FormatWith(settings.Vendor, typeof(TInput).Name).ToLower());
+            getRequest = getRequest ?? DefaultGetRequest;
 
-            var request = new HttpRequestMessage(HttpMethod.Get, settings.Path + "/{0}".FormatWith(typeof(TInput).Name).ToLower())
-            {
-                Content = httpContent,
-            };
-            
-            request.Headers.Accept.ParseAdd("application/vnd.{0}.{1}+json".FormatWith(settings.Vendor, typeof(TOutput).Name).ToLower());
+            var request = getRequest(input, typeof(TInput), typeof(TOutput), settings);
             
             HttpResponseMessage response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
 
             await response.ThrowOnErrorStatus(request, settings);
 
             var jsonString = await response.Content.ReadAsStringAsync();
+
             return (TOutput)settings.Serializer.Deserialize(jsonString, typeof (TOutput));
         }
+
+        public static readonly Func<object, Type, Type, IMessageExecutionSettings, HttpRequestMessage>
+            DefaultGetRequest = (input, inputType, outputType, settings) =>
+            {
+                string queryJson = settings.Serializer.Serialize(input);
+                var httpContent = new StringContent(queryJson);
+                httpContent.Headers.ContentType =
+                    MediaTypeHeaderValue.Parse(
+                        "application/vnd.{0}.{1}+json".FormatWith(settings.Vendor, inputType.Name).ToLower());
+
+                var request = new HttpRequestMessage(HttpMethod.Get,
+                    settings.Path + "/{0}".FormatWith(inputType.Name).ToLower())
+                {
+                    Content = httpContent,
+                };
+
+                request.Headers.Accept.ParseAdd(
+                    "application/vnd.{0}.{1}+json".FormatWith(settings.Vendor, outputType.Name).ToLower());
+
+                return request;
+            };
     }
 }
