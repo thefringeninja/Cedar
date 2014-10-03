7907e799e12e6042d334c77dfad3bd5d6e22a25b
thefringeninja <joao.paul@braganca.name>
2014-10-03 15:04:36 +0200

    supporting string on overloads

--- src/Cedar/Domain/Persistence/RepositoryExtensions.cs
+++ src/Cedar/Domain/Persistence/RepositoryExtensions.cs
@@ -45,6 +45,42 @@
             return repository.GetById<TAggregate>(bucketId, id, int.MaxValue, cancellationToken);
         }
 
+        public static Task<TAggregate> GetById<TAggregate>([NotNull] this IRepository repository, string id, CancellationToken cancellationToken)
+            where TAggregate : class, IAggregate
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException("repository");
+            }
+
+            return GetById<TAggregate>(repository, Bucket.Default, id, cancellationToken);
+        }
+
+        public static Task<TAggregate> GetById<TAggregate>([NotNull] this IRepository repository, string id, int version, CancellationToken cancellationToken)
+            where TAggregate : class, IAggregate
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException("repository");
+            }
+
+            return repository.GetById<TAggregate>(Bucket.Default, id, version, cancellationToken);
+        }
+
+        public static Task<TAggregate> GetById<TAggregate>(
+            [NotNull] this IRepository repository,
+            string bucketId, string id,
+            CancellationToken cancellationToken)
+            where TAggregate : class, IAggregate
+        {
+            if (repository == null)
+            {
+                throw new ArgumentNullException("repository");
+            }
+
+            return repository.GetById<TAggregate>(bucketId, id, int.MaxValue, cancellationToken);
+        }
+
         public static Task Save([NotNull] this IRepository repository, IAggregate aggregate, Guid commitId, CancellationToken cancellationToken)
         {
             if (repository == null)
