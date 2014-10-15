ef7fbe468afabfb027ff5b5e2699249285a354cc
thefringeninja <joao.paul@braganca.name>
2014-10-15 21:01:09 +0200

    enumerable tests

---
+++ src/Cedar.Tests/Testing/EnumerableTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Cedar.Testing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class EnumerableTests
+    {
+        public IEnumerable<Task<ScenarioResult>> test_method()
+        {
+            return Enumerable.Range(0, 5)
+                .Select(async _ => await Scenario.For<DateTime>()
+                    .Given(new DateTime(2000, 1, 1))
+                    .When(date => date.AddDays(1))
+                    .ThenShouldEqual(new DateTime(2000, 1, 1)));
+        }
+    }
+}
