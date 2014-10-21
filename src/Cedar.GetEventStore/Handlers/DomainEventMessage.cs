f13c0cf6c069ed0797f50be9a1f2484797350cd8
thefringeninja <joao.paul@braganca.name>
2014-10-21 17:01:25 +0200

    ges needs its own version of the DEM

---
+++ src/Cedar.GetEventStore/Handlers/DomainEventMessage.cs
@@ -0,0 +1,40 @@
+﻿namespace Cedar.Handlers
+{
+    using System.Collections.Generic;
+
+    public class DomainEventMessage<T>
+    {
+        private readonly T _domainEvent;
+        private readonly string _streamId;
+        private readonly int _checkpoint;
+        private readonly IDictionary<string, object> _headers;
+
+        public DomainEventMessage(T domainEvent, IDictionary<string, object> headers, string streamId, int checkpoint)
+        {
+            _domainEvent = domainEvent;
+            _streamId = streamId;
+            _checkpoint = checkpoint;
+            _headers = headers;
+        }
+
+        public T DomainEvent
+        {
+            get { return _domainEvent; }
+        }
+
+        public string StreamId
+        {
+            get { return _streamId; }
+        }
+
+        public int Checkpoint
+        {
+            get { return _checkpoint; }
+        }
+
+        public IDictionary<string, object> Headers
+        {
+            get { return _headers; }
+        }
+    }
+}
