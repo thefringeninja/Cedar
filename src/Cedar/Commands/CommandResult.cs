d85ce9020bee43912ddd04b7f6ab35d7209970d7
thefringeninja <joao.paul@braganca.name>
2014-11-12 11:44:43 +0100

    read model for the result of command processing

---
+++ src/Cedar/Commands/CommandResult.cs
@@ -0,0 +1,65 @@
+﻿namespace Cedar.Commands
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading;
+    using Cedar.Annotations;
+    using Cedar.ExceptionModels.Client;
+    using Cedar.Handlers;
+
+    public class CommandResult
+    {
+        private readonly Guid _commitId;
+        private readonly IHandlerResolver _handlerResolver;
+
+        public Guid CommitId
+        {
+            get { return _commitId; }
+        }
+
+        public ExceptionModel Exception { get; private set; }
+
+        private int _handlerCount;
+        private int _successfulHandlerCount;
+        private int _unsuccessfulHandlerCount;
+        private static readonly MethodInfo NotifyEventWrittenInternalMethod;
+
+        public CommandResult(Guid commitId, IHandlerResolver handlerResolver)
+        {
+            _commitId = commitId;
+            _handlerResolver = handlerResolver;
+        }
+
+        static CommandResult()
+        {
+            NotifyEventWrittenInternalMethod = typeof(CommandResult).GetMethod("NotifyEventWrittenInternal",
+                BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+
+        public void NotifyEventWritten(DomainEventMessage @event)
+        {
+            NotifyEventWrittenInternalMethod
+                .MakeGenericMethod(@event.DomainEvent.GetType())
+                .Invoke(this, new[] {@event});
+        }
+
+        [UsedImplicitly]
+        private void NotifyEventWrittenInternal<TEvent>(DomainEventMessage<TEvent> @event)
+            where TEvent : class
+        {
+            Interlocked.Add(ref _handlerCount, _handlerResolver.GetHandlersFor<DomainEventMessage<TEvent>>().Count());
+        }
+
+        public void NotifyEventHandledSuccessfully()
+        {
+            Interlocked.Increment(ref _successfulHandlerCount);
+        }
+
+
+        public void NotifyEventHandledUnsuccessfully()
+        {
+            Interlocked.Increment(ref _unsuccessfulHandlerCount);
+        }
+    }
+}
---
+++ src/Cedar/Commands/CommandResultHandler.cs
@@ -0,0 +1,62 @@
+﻿namespace Cedar.Commands
+{
+    using System;
+    using System.Collections.Concurrent;
+    using Cedar.Handlers;
+
+    public class CommandResultHandler
+    {
+        private readonly IHandlerResolver _handlerResolver;
+        private readonly ConcurrentDictionary<Guid, CommandResult> _storage;
+
+        public CommandResultHandler(IHandlerResolver handlerResolver, ConcurrentDictionary<Guid, CommandResult> storage)
+        {
+            _handlerResolver = handlerResolver;
+            _storage = storage;
+        }
+
+        public void Handle(DomainEventMessage message)
+        {
+            var commitId = message.GetCommitId();
+
+            if(false == commitId.HasValue)
+                return;
+
+            var result = GetOrAddCommandResult(commitId.Value);
+
+            result.NotifyEventWritten(message);
+        }
+
+        public void Handle(EventHandled message)
+        {
+            var commitId = message.CommitId;
+
+            var result = GetOrAddCommandResult(commitId);
+
+            if(message.Successful)
+            {
+                result.NotifyEventHandledSuccessfully();
+            }
+            else
+            {
+                result.NotifyEventHandledUnsuccessfully();
+            }
+        }
+
+
+
+        private CommandResult GetOrAddCommandResult(Guid commitId)
+        {
+            CommandResult result;
+            if (false == _storage.TryGetValue(commitId, out result))
+            {
+                result = new CommandResult(commitId, _handlerResolver);
+
+                _storage.TryAdd(commitId, result);
+            }
+            return result;
+        }
+
+
+    }
+}
