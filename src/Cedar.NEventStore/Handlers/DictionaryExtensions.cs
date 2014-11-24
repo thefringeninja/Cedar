5ffb16b925e08f5b4021a697263d9dad9139809f
Damian Hickey <dhickey@gmail.com>
2014-11-24 23:42:22 +0100

    Moved a extension methods class to where it's used.

--- src/Cedar.NEventStore/Handlers/DictionaryExtensions.cs
+++ src/Cedar.NEventStore/Handlers/DictionaryExtensions.cs
@@ -1,22 +1,21 @@
-namespace Cedar.Handlers
+namespace Cedar.NEventStore.Handlers
 {
     using System.Collections.Generic;
     using System.Linq;
 
-    public static class DictionaryExtensions
+    internal static class DictionaryExtensions
     {
-        public static IDictionary<string, object> Merge(this IDictionary<string, object> target,
+        public static IDictionary<string, object> Merge(
+            this IDictionary<string, object> target,
             params IDictionary<string, object>[] others)
         {
             return target.Merge(others.AsEnumerable());
         }
 
-        public static IDictionary<string, object> Merge(this IDictionary<string, object> target,
+        private static IDictionary<string, object> Merge(
+            this IDictionary<string, object> target,
             IEnumerable<IDictionary<string, object>> others)
         {
-            Guard.EnsureNotNull(target, "target");
-            Guard.EnsureNotNull(others, "others");
-
             foreach(var pair in others.SelectMany(other => other))
             {
                 target[pair.Key] = pair.Value;
