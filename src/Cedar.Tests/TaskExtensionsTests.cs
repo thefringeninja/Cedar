f4009603ede0680956aa4f37a3503cc984c061d4
Damian Hickey <dhickey@gmail.com>
2014-11-26 16:02:48 +0100

    Task extensions tests.

---
+++ src/Cedar.Tests/TaskExtensionsTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Cedar
+{
+    using System;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Xunit;
+
+    public class TaskExtensionsTests
+    {
+        [Fact]
+        public void Should_timeout()
+        {
+            Func<Task> act = () => Task.Delay(10000).WithTimeout(TimeSpan.FromMilliseconds(1));
+
+            act.ShouldThrow<TimeoutException>();
+        }
+    }
+}
--- src/Cedar/TaskExtensions.cs
+++ src/Cedar/TaskExtensions.cs
@@ -1,45 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cedar
+﻿namespace Cedar
 {
+    using System;
     using System.Threading;
+    using System.Threading.Tasks;
+    using Cedar.Internal;
 
     public static class TaskExtensions
     {
         public static async Task<TResult> WithTimeout<TResult>(this Task<TResult> task, TimeSpan delay)
         {
             var cts = new CancellationTokenSource();
-
-            var completedTask = await Task.WhenAny(task, Task.Delay(delay, cts.Token));
-
+            Task completedTask = await Task.WhenAny(task, Task.Delay(delay, cts.Token));
             if(completedTask != task)
             {
                 throw new TimeoutException("The operation has timed out.");
             }
-
             cts.Cancel();
-
-            return await task;
+            return await task.NotOnCapturedContext();
         }
 
         public static async Task WithTimeout(this Task task, TimeSpan delay)
         {
             var cts = new CancellationTokenSource();
-
-            var completedTask = await Task.WhenAny(task, Task.Delay(delay, cts.Token));
-
-            if (completedTask != task)
+            Task completedTask = await Task.WhenAny(task, Task.Delay(delay, cts.Token));
+            if(completedTask != task)
             {
                 throw new TimeoutException("The operation has timed out.");
             }
-
             cts.Cancel();
-
-            await task;
+            await task.NotOnCapturedContext();
         }
     }
 }
