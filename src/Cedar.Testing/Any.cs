82959e1cd9641aca4b9e904bde72bdc1641137b8
thefringeninja <joao.paul@braganca.name>
2014-12-11 13:48:32 +0100

    shouldn't just be a random guid!

--- src/Cedar.Testing/Any.cs
+++ src/Cedar.Testing/Any.cs
@@ -1,10 +1,11 @@
 namespace Cedar.Testing
 {
     using System;
+    using System.Linq;
 
     public static class Any
     {
         public static readonly DateTime Date = DateTime.MaxValue.AddSeconds(-60);
-        public static readonly Guid Guid = Guid.NewGuid();
+        public static readonly Guid Guid = new Guid(Enumerable.Repeat((byte)0xff, 16).ToArray());
     }
 }
