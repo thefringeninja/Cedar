8c96f5da149c634365df8300d5afcf49f7f008c3
Damian Hickey <dhickey@gmail.com>
2014-12-22 13:00:44 +0100

    Merge two TaskExtensions classes into one and set the namespace to System.Threading.Tasks

--- src/Cedar/Internal/TaskExtensions.cs
+++ src/Cedar/Internal/TaskExtensions.cs
@@ -1,19 +1,44 @@
 ﻿// ReSharper disable once CheckNamespace
-namespace Cedar.Internal
+namespace System.Threading.Tasks
 {
+    using System;
     using System.Runtime.CompilerServices;
-    using System.Threading.Tasks;
+    using System.Threading;
 
     internal static class TaskExtensions
     {
-        public static ConfiguredTaskAwaitable<T> NotOnCapturedContext<T>(this Task<T> task)
+        internal static ConfiguredTaskAwaitable<T> NotOnCapturedContext<T>(this Task<T> task)
         {
             return task.ConfigureAwait(false);
         }
 
-        public static ConfiguredTaskAwaitable NotOnCapturedContext(this Task task)
+        internal static ConfiguredTaskAwaitable NotOnCapturedContext(this Task task)
         {
             return task.ConfigureAwait(false);
+        }
+
+        internal static async Task<TResult> WithTimeout<TResult>(this Task<TResult> task, TimeSpan delay)
+        {
+            var cts = new CancellationTokenSource();
+            Task completedTask = await Task.WhenAny(task, Task.Delay(delay, cts.Token));
+            if (completedTask != task)
+            {
+                throw new TimeoutException("The operation has timed out.");
+            }
+            cts.Cancel();
+            return await task.NotOnCapturedContext();
+        }
+
+        internal static async Task WithTimeout(this Task task, TimeSpan delay)
+        {
+            var cts = new CancellationTokenSource();
+            Task completedTask = await Task.WhenAny(task, Task.Delay(delay, cts.Token));
+            if (completedTask != task)
+            {
+                throw new TimeoutException("The operation has timed out.");
+            }
+            cts.Cancel();
+            await task.NotOnCapturedContext();
         }
     }
 }
