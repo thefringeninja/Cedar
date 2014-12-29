d74b83adfc633a03f47892937984796a3eac01b9
Damian Hickey <dhickey@gmail.com>
2014-12-29 11:24:46 +0100

    Some xml comments

--- src/Cedar/TypeResolution/TryResolveType.cs
+++ src/Cedar/TypeResolution/TryResolveType.cs
@@ -2,5 +2,11 @@ namespace Cedar.TypeResolution
 {
     using System;
 
+    /// <summary>
+    ///     A delegate to try to resolve a <see cref="Type"/> from a parsed media type.
+    /// </summary>
+    /// <param name="parsedMediaType">The parsed media type.</param>
+    /// <param name="resolvedType">The resolved type.</param>
+    /// <returns>true if a type is found; otherwise false.</returns>
     public delegate bool TryResolveType(IParsedMediaType parsedMediaType, out Type resolvedType);
 }
