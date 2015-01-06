89280b570fbdf186014b3a9b6cb43c36d853b341
Damian Hickey <dhickey@gmail.com>
2015-01-06 20:26:34 +0100

    Redundant directive.

--- src/Cedar.Tests/Handlers/ProcessManagerHandlerTests.cs
+++ src/Cedar.Tests/Handlers/ProcessManagerHandlerTests.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Net;
-    using System.Reactive.Concurrency;
     using System.Reactive.Linq;
     using System.Reactive.Threading.Tasks;
     using System.Security.Claims;
