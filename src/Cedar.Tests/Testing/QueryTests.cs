3fb5254ec39d3a05d40d7d47dda2fac2b570df25
Damian Hickey <dhickey@gmail.com>
2015-01-15 20:09:48 +0100

    Unused directives.

--- src/Cedar.Tests/Testing/QueryTests.cs
+++ src/Cedar.Tests/Testing/QueryTests.cs
@@ -1,8 +1,6 @@
 ﻿namespace Cedar.Testing
 {
-    using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
     using System.Net.Http;
     using System.Text;
     using System.Threading.Tasks;
