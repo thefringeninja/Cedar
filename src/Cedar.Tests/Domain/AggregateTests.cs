7542e8f89fadbaf076ca20b4f019bcf0d42673ee
Damian Hickey <dhickey@gmail.com>
2015-01-22 21:45:51 +0100

    Make test more concise

--- src/Cedar.Tests/Domain/AggregateTests.cs
+++ src/Cedar.Tests/Domain/AggregateTests.cs
@@ -5,58 +5,29 @@
 
     public class AggregateTests
     {
-        class SomethingHappened
-        {
-            public override string ToString()
-            {
-                return "Something happened.";
-            }
-        }
-
-        class SomethingElseHappened
-        {
-            public override string ToString()
-            {
-                return "Something else happened.";
-            }
-        }
-
-        class Aggregate : AggregateBase
-        {
-            private int _something = 0;
-
-            public Aggregate(string id)
-                : base(id)
-            {
-
-            }
-
-            void Apply(SomethingHappened e)
-            {
-                _something++;
-            }
-
-            public void DoSomething()
-            {
-                RaiseEvent(new SomethingHappened());
-            }
-
-            public void DoSomethingElse()
-            {
-                RaiseEvent(new SomethingElseHappened());
-            }
-
-        }
-
         [Fact]
         public void should_not_require_an_explicit_handler()
         {
             var aggregate = new Aggregate("id");
 
-            aggregate.DoSomething();
-            aggregate.DoSomethingElse();
+            aggregate.Command();
 
-            aggregate.Version.Should().Be(2);
+            aggregate.Version.Should().Be(1);
+        }
+
+        private class EventThatDoesNotHaveAnApply
+        {}
+
+        private class Aggregate : AggregateBase
+        {
+            public Aggregate(string id)
+                : base(id)
+            {}
+
+            public void Command()
+            {
+                RaiseEvent(new EventThatDoesNotHaveAnApply());
+            }
         }
     }
 }
