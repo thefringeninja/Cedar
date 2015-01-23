679953454d47d631e80750a304ed03b8e4327bfb
Damian Hickey <dhickey@gmail.com>
2015-01-23 08:00:16 +0100

    Better transient retry policy example.

--- src/Cedar.Example/Handlers/TransientExceptionHandling.cs
+++ src/Cedar.Example/Handlers/TransientExceptionHandling.cs
@@ -10,6 +10,8 @@
         public TransientExceptionHandling()
         {
             var handlerModule = new HandlerModule();
+
+            // 1. Explicitly defining a retry policy in a handler
             handlerModule
                 .For<Event>()
                 .Handle((message, ct) =>
@@ -18,9 +20,34 @@
                         .RetryForeverAsync(ex => { /* log message */})
                         .ExecuteAsync(() => /* some async op */ Task.FromResult(0));
                 });
+
+            // 2. Re-using a policy via extension methods.
+            handlerModule
+                .For<Event>()
+                .ConnectionTimeoutRetry()
+                .Handle((message, ct) => Task.FromResult(0));
         }
 
         public class Event
         {}
+
+    }
+
+    public static class HandlerBuilderExtensions
+    {
+        public static IHandlerBuilder<T> ConnectionTimeoutRetry<T>(
+            this IHandlerBuilder<T> handlerBuilder)
+            where T : class
+        {
+            return handlerBuilder.Pipe(next => (message, ct) =>
+            {
+                return Policy.Handle<TimeoutException>()
+                    .RetryForeverAsync(ex =>
+                    {
+                        /* log message, system warning etc */
+                    })
+                    .ExecuteAsync(() => next(message, ct));
+            });
+        }
     }
 }
