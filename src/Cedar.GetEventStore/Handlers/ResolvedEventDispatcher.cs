7422f9db6bb3dae09da2ddb20b223ce3cb9dd2e8
thefringeninja <joao.paul@braganca.name>
2015-01-30 17:18:37 +0100

    proper-er logging

--- src/Cedar.GetEventStore/Handlers/ResolvedEventDispatcher.cs
+++ src/Cedar.GetEventStore/Handlers/ResolvedEventDispatcher.cs
@@ -8,6 +8,7 @@
     using System.Threading;
     using System.Threading.Tasks;
     using Cedar.Annotations;
+    using Cedar.GetEventStore.Logging;
     using Cedar.GetEventStore.Serialization;
     using Cedar.Handlers;
     using Cedar.Internal;
@@ -179,6 +180,12 @@
             private readonly CancellationToken _token;
             private readonly ConcurrentQueue<ResolvedEvent> _events;
             private readonly InterlockedBoolean _isPushing;
+            private static readonly ILog Logger;
+
+            static SimpleQueue()
+            {
+                Logger = LogProvider.For<SimpleQueue>();
+            }
 
             public SimpleQueue(Func<ResolvedEvent, CancellationToken, Task> onResolvedEvent, CancellationToken token)
             {
@@ -211,7 +218,7 @@
                         }
                         catch(Exception ex)
                         {
-                            Console.WriteLine(ex.ToString());
+                            Logger.ErrorException(ex.Message, ex);
                         }
                     }
                     _isPushing.Set(false);
