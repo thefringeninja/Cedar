b58c579853e1a324f2ef5195b407a5174f7b8aeb
Damian Hickey <dhickey@gmail.com>
2015-02-08 23:00:42 +0100

    Extension not used.

--- src/Cedar/Handlers/EventMessageHeaders.cs
+++ src/Cedar/Handlers/EventMessageHeaders.cs
@@ -1,25 +1,10 @@
 ﻿namespace Cedar.Handlers
 {
-    using System;
-
     public static class EventMessageHeaders
     {
         public const string StreamId = "StreamId";
         public const string CommitId = "CommitId";
         public const string Type = "Type";
         public const string Timestamp = "Timestamp";
-
-        public static Guid? GetCommitId(this EventMessage @event)
-        {
-            object commitIdValue;
-            Guid commitId;
-            if(!@event.Headers.TryGetValue(CommitId, out commitIdValue)
-               || commitIdValue == null
-               || !Guid.TryParse(commitIdValue.ToString(), out commitId))
-            {
-                return default(Guid?);
-            }
-            return commitId;
-        }
     }
 }
