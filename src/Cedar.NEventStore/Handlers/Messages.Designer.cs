e5af2f12c4a01f3448673f97f07000952d8dd35d
Damian Hickey <dhickey@gmail.com>
2015-02-08 21:15:31 +0100

    These messages are not used.

--- src/Cedar.NEventStore/Handlers/Messages.Designer.cs
+++ src/Cedar.NEventStore/Handlers/Messages.Designer.cs
@@ -9,14 +9,9 @@
 //------------------------------------------------------------------------------
 
 namespace Cedar.NEventStore.Handlers {
-    using System.CodeDom.Compiler;
-    using System.ComponentModel;
-    using System.Diagnostics;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Globalization;
-    using System.Resources;
-    using System.Runtime.CompilerServices;
-
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -24,27 +19,27 @@ namespace Cedar.NEventStore.Handlers {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
-    [DebuggerNonUserCode()]
-    [CompilerGenerated()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Messages {
         
-        private static ResourceManager resourceMan;
+        private static global::System.Resources.ResourceManager resourceMan;
         
-        private static CultureInfo resourceCulture;
+        private static global::System.Globalization.CultureInfo resourceCulture;
         
-        [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal Messages() {
         }
         
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Advanced)]
-        internal static ResourceManager ResourceManager {
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Resources.ResourceManager ResourceManager {
             get {
-                if (ReferenceEquals(resourceMan, null)) {
-                    ResourceManager temp = new ResourceManager("Cedar.NEventStore.Handlers.Messages", typeof(Messages).Assembly);
+                if (object.ReferenceEquals(resourceMan, null)) {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Cedar.NEventStore.Handlers.Messages", typeof(Messages).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -55,8 +50,8 @@ namespace Cedar.NEventStore.Handlers {
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
-        [EditorBrowsable(EditorBrowsableState.Advanced)]
-        internal static CultureInfo Culture {
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -71,42 +66,6 @@ namespace Cedar.NEventStore.Handlers {
         internal static string ExceptionHasOccuredWhenDispatchingACommit {
             get {
                 return ResourceManager.GetString("ExceptionHasOccuredWhenDispatchingACommit", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Duration cannot be negative..
-        /// </summary>
-        internal static string NegativeDuration {
-            get {
-                return ResourceManager.GetString("NegativeDuration", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to Retry interval cannot be negative..
-        /// </summary>
-        internal static string NegativeRetryInterval {
-            get {
-                return ResourceManager.GetString("NegativeRetryInterval", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to A TransientException has occured {0} times and exceeded the retry policy duration of {0}..
-        /// </summary>
-        internal static string TransientExceptionExceededDuration {
-            get {
-                return ResourceManager.GetString("TransientExceptionExceededDuration", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to A TransientException has occured {0} times..
-        /// </summary>
-        internal static string TransientExceptionOccured {
-            get {
-                return ResourceManager.GetString("TransientExceptionOccured", resourceCulture);
             }
         }
     }
