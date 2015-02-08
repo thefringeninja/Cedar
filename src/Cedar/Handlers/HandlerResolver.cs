437580a0f248dd20454e958b2db4ae33a961c1b9
Damian Hickey <dhickey@gmail.com>
2015-02-08 22:00:00 +0100

    Don't need a container

--- src/Cedar/Handlers/HandlerResolver.cs
+++ src/Cedar/Handlers/HandlerResolver.cs
@@ -1,28 +1,40 @@
 ﻿namespace Cedar.Handlers
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using TinyIoC;
 
     public class HandlerResolver : IHandlerResolver
     {
         private readonly TinyIoCContainer _container = new TinyIoCContainer();
+        private readonly Dictionary<Type, List<object>> _handlers = new Dictionary<Type, List<object>>(); 
 
         public HandlerResolver(params HandlerModule[] handlerModules)
         {
             foreach(var module in handlerModules)
             {
-                foreach(var handlerRegistration in module.HandlerRegistrations)
+                foreach(var registration in module.HandlerRegistrations)
                 {
                     _container.Register(
-                        handlerRegistration.RegistrationType,
-                        handlerRegistration.HandlerInstance);
+                        registration.RegistrationType,
+                        registration.HandlerInstance);
+
+                    List<object> handlers;
+                    if(!_handlers.TryGetValue(registration.RegistrationType, out handlers))
+                    {
+                        handlers = new List<object>();
+                    }
+                    handlers.Add(registration.HandlerInstance);
+                    _handlers[registration.RegistrationType] = handlers;
                 }
             }
         }
 
         public IEnumerable<Handler<TMessage>> ResolveAll<TMessage>() where TMessage : class
         {
-            return _container.ResolveAll<Handler<TMessage>>();
+            return _handlers[typeof(Handler<TMessage>)]
+                .Select(handler => (Handler<TMessage>) handler);
         }
     }
 }
